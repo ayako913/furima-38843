9dce9c21f039cece3966dc44672835374fb045bc
ayako913 <121155701+ayako913@users.noreply.github.com>
2023-01-19 11:46:07 +0900

    ユーザー新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :firstkana, presence: true
+  validates :lastkana, presence: true
+  validates :birthday, presence: true
 end

1cd141921db6e5158b0546a3af0330e2e03346d9
ayako913 <121155701+ayako913@users.noreply.github.com>
2023-01-20 20:31:50 +0900

    item.jsの変数名変更

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,11 +1,11 @@
 function priceBuy(){
   const price = document.getElementById("item-price");
   price.addEventListener("keyup", () => {
-    const tax = 0.1;
+    const commission = 0.1;
     const salesTax = document.getElementById("add-tax-price");
-    salesTax.innerHTML = Math.floor(price.value * tax);
+    salesTax.innerHTML = Math.floor(price.value * commission);
     const profit = document.getElementById("profit");
-    profit.innerHTML = price.value - Math.floor(price.value * tax);
+    profit.innerHTML = price.value - Math.floor(price.value * commission);
   });
 };
 window.addEventListener('load', priceBuy);

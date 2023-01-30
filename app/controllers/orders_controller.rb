5358043d4beb431940c035bf958a985247288871
ayako913 <121155701+ayako913@users.noreply.github.com>
2023-01-30 14:53:20 +0900

    条件分岐の追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+
   def index
     @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new
+    if current_user.id == @item.user_id || @item.order.present? 
+      redirect_to root_path
+    end
   end
 
   def create
